74e1d97eed2e33d8e0b612d819aad489a1c79726
ben <ouo.joy1028@gmail.com>
2025-12-27 01:15:15 +0800

    fix: 🐛 edit gamemode

--- model/GameMode.java
+++ model/GameMode.java
@@ -2,7 +2,7 @@ package model;
 
 public enum GameMode {
     TIME_ATTACK,     // 限時純計分
-    CLEAR_40,        // 計時消40行
-    COMBO_4W,        // 4格寬 combo 模式
-    SETTINGS         // 設定
+    CLEAR_40,        // 計時消40行 (先不做)
+    COMBO_4W,        // 4格寬 combo 模式 (先不做)
+    SETTINGS         // 設定 (先不做)
 }
